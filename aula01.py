8354440bc5c19454b0fff4e151d866425a372484
CEZAR DA SILVA SOUZA <158162683+cezarsillva@users.noreply.github.com>
2024-09-27 13:25:23 +0000

    Salvo

---
+++ aula01.py
@@ -0,0 +1,54 @@
+# print("Olá mundo")
+# print("Vamos começar com a graça de Deus mais um semestre.")
+
+# github.com/jppreti/documents
+
+
+# Para executar os dados no python digite " python3 + o nome da pagina" que ao terminar a execução finaliza o programa prem quando digitamos o - i contunua em execução.
+
+
+
+nome = 'João' # Variavel Global
+# Criando o comando olá
+
+def olá (nome): # Variavel local
+ 
+    
+    print(f'Olá {nome} ! Seja bem Vindo(a) !')
+    
+
+
+print(f'Olá {nome}!')
+
+olá ('João')
+
+olá (input('Digite seu nome: '))
+
+
+
+
+
+
+
+
+
+# nome1 = 'Cezar da silva souza'
+
+# idade1 = 45
+# altura1 = 1.85
+
+# nome2 = 'João Paulo Preti'
+
+# idade2 = 36
+# altura2 = 1.65
+
+# print(f'Olá {nome1}! Seja Bem Vindo.')
+
+
+# print(f'Idade {idade1}')
+# print(f'Altura {altura1}')
+
+# print(f'Olá {nome2}! Seja Bem Vindo.')
+
+# print(f'Idade {idade2}')
+# print(f'Altura {altura2}')
