f78e5bc6fd76642deec33346e3b1520e00329342
CEZAR DA SILVA SOUZA <158162683+cezarsillva@users.noreply.github.com>
2024-10-01 14:40:35 +0000

    Lista1

--- matematica.py
+++ matematica.py
@@ -1,17 +1,26 @@
 
+#https://github.com/ifmt-cba/lp20242
+
+
+
+#def area(largura, profundidade):
+    #return largura * profundidade
+
+
+#def volume(largura, profundidade, altura):
+    #return area(largura, profundidade) * altura
+
+#print(area(2,3))
+#print(volume(2,3,5))
+
+#largura = input("Digite a largura da area: ")
+#profundidade= input("Digite a largura da area: ")
+#print(area(int(largura),int(profundidade)))
 
 
 
 
-
-
-def area(largura, profundidade):
-    return largura * profundidade
-
-
-def volume(largura, profundidade, altura):
-    return area(largura, profundidade) * altura
-
-print(area(2,3))
-print(volume(2,3,5))
-
+#notas = [7.5, 8.2, 6.9, 9.1, 7.8]
+#soma = sum(notas)
+#media = soma / len(notas)
+#print("A média das notas dos alunos é:", media)
