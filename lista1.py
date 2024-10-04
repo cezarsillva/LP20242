4f17f02973cf78a590876c3f72cd866409ef24cc
CEZAR DA SILVA SOUZA <158162683+cezarsillva@users.noreply.github.com>
2024-10-04 10:46:55 +0000

    pronto

--- lista1.py
+++ lista1.py
@@ -1,154 +1,280 @@
-'''
-Exercícios sobre os comandos básicos em Python
-'''
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
+#-----------------------------Lista 1-------------------------------------#
+
+
+
+
+
+#print(f'------------------------------------------------------------------------') 
+
 #1. Faça um programa que imprima o seu nome.
 
+#print(f'exercicios 1') 
 
-#nome = str(input ('Digite seu nome: '))
+
+#nome = input ('Digite seu nome: ')
 #print(f'Olá {nome}. Seja bem vindo(a)!')
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
+#print(f'------------------------------------------------------------------------') 
 
 #2. Faça um programa que imprima o produto dos valores 30 e 27.
 
+#valor1 = int(input ('Digite o primeiro valor:'))
+#valor2 = int(input ('Digite o segundo valor:'))
 
-#valor1 = int(input('Digite o primeiro valor: '))
-#valor2 = int(input('Digite o segundo valor: '))
-
-#produto = (valor1*valor2)
-
+#produto = (valor1 * valor2)
 
 #print(f'O valor do produto é : {produto}')
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+#print(f'------------------------------------------------------------------------') 
+
 
 #3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
 
-#num1 = int(input('Digite o primeiro número: '))
-#num2 = int(input('Digite o segundo número: '))
-#num3 = int(input('Digite o terceiro número: '))
+
+#valor1 = int(input ('Digite o primeiro valor:'))
+#valor2 = int(input ('Digite o segundo valor:'))
+#valor3 = int(input ('Digite o segundo valor:'))
 
 
-#media = ((num1 + num2 + num3) / 3)
+#Média = ((valor1 + valor2 + valor3)/ 3)
 
 
-#print(f'A média entre os números é: {media}')
+#print(f'A média é : {Média}')
 
+#print(f'------------------------------------------------------------------------') 
 
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
 #4. Faça um programa que leia e imprima um número inteiro.
 
-#numero = int(input('Digite um numero inteiro: '))
 
-#print(f'O número inteiro é: {numero}')
+#numero= int(input ('Digite um numero inteiro:'))
 
+#print(numero)
 
+#print(f'------------------------------------------------------------------------') 
 
-
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
 #5. Faça um programa que leia dois números reais e os imprima.
 
 
-#numero1 = float(input('Digite o primeiro número real: '))
-#numero2 = float(input('Digite o segundo número real: '))
+#numero1= int(input ('Digite o primeiro numero:'))
+#numero2 = int(input ('Digite o segundo numero:'))
 
 
+#print(f'O primeiro número é: {numero1} O segundo número é: {numero2}')
 
-#print(f'O primeiro número real é: {numero1}. E o segundo número real é: {numero2}.')
+#print(f'------------------------------------------------------------------------') 
 
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
 #6. Faça um programa que leia um número inteiro e imprima o seu
 #   antecessor e o seu sucessor.
 
+#numero = int(input ('Digite o valor: '))
 
 
-#numero = int(input('Digite um número: '))
+#print(f'{numero + 1}')
+#print(f' {numero - 1}')
 
-
-
-#print(f'O número Antecessor é: {numero - 1}')
-#print(f'O número Sucessor é: {numero + 1}')
-
-
-
-
-
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
+#print(f'------------------------------------------------------------------------') 
 
 #7. Faça um programa que leia o nome o endereço e o telefone de
 #   um cliente e ao final, imprima esses dados.
 
 
-endereço = str(input('Digite o endereço do Cliente: '))
-telefone = int(input('Digite o telefone do Cliente: '))
 
 
-print(f'O endereço do cliente é: {endereço}')
-print(f'O telefone do cliente é: {telefone}')
+#nome= str(input ('Digite o nome do cliente: '))
+#endereço = str(input ('Digite o endereço do cliente : '))
+#telefone = int(input ('Digite o telefone do cliente: '))
+
+#print("\n")
+
+#print(f'O nome do cliente é : {nome}\n')
+
+#print(f'O endereço do cliente é : {endereço}\n')
+
+#print(f'O telefone do cliente é : {telefone}\n')
 
 
+#print(f'------------------------------------------------------------------------') 
 
 
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
 #8. Faça um programa que leia dois números inteiros e imprima a
 #   subtração deles.
 
 
+#numero1= int(input ('Digite o primeiro numero:'))
+#numero2 = int(input ('Digite o segundo numero:'))
 
-num1 = int(input('Digite o primeiro número: '))
-num2 = 
+#subtração = (numero1 - numero2)
 
-
-
-
-
+#print(f'A subtração dos dois números são: {subtração}')
 
 
+#print(f'------------------------------------------------------------------------') 
 
-
-
-
-#------------------------------------------------------------------------------------------------------------------------------------------------------#
 #9. Faça um programa que leia um número real e imprima ¼ deste número.
+
+#numero = float(input('Digite um número: '))
+
+#fração = numero * (1/4)
+
+#print(f'A quarta parte do numero {numero} é : {fração}')
+
+#print(f'------------------------------------------------------------------------')
 
 #10. Faça um programa que leia três números reais e calcule a
 #    média aritmética destes números. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
+
+
+#nota1 = int(input ('Digite o primeiro valor:'))
+#nota2 = int(input ('Digite o segundo valor:'))
+#nota3 = int(input ('Digite o segundo valor:'))
+
+
+#Média = ((nota1 + nota2 + nota3)/ 3)
+
+#print(f'A média é : {Média}')
+
+
+#print(f'------------------------------------------------------------------------')
 
 #11. Faça um programa que leia dois números reais e calcule as
 #    quatro operações básicas entre estes dois números, adição,
 #    subtração,multiplicação e divisão. Ao final, o programa
 #    deve imprimir os resultados dos cálculos.
 
+
+
+#numero1= int(input ('Digite o primeiro numero:'))
+#numero2 = int(input ('Digite o segundo numero:'))
+
+#adição = (numero1 + numero2)
+#subtração = (numero1 - numero2)
+#multiplicação = (numero1 * numero2)
+#divisão = (numero1 / numero2)
+
+#print("\n")
+
+#print(f'A adição dos dois números é: {adição}')
+#print(f'A subtração dos dois números é: {subtração}')
+#print(f'A multiplicação dos dois números é: {multiplicação}')
+#print(f'A divisão dos dois números é: {divisão}')
+
+#print("\n")
+
+#print(f'------------------------------------------------------------------------')
+
 #12. Faça um programa que leia um número real e calcule o
 #    quadrado deste número. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
 
+
+#numero= int(input ('Digite o primeiro numero:'))
+
+
+#quadrado = (numero ** 2)
+
+#print("\n")
+
+#print(f'o quadrado de {numero}: {quadrado}')
+
+#print("\n")
+
+#print(f'------------------------------------------------------------------------')
+
 #13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
 
+
+#saldo = float(input ('Digite o valor de saldo :'))
+#taxa = float(input ('Digite o valor da taxa:'))
+
+#print("\n")
+
+#novosaldo = saldo + (saldo * (taxa / 100))
+
+#print(f'O novo saldo com o acréscimo de {taxa} é: R$ {novosaldo}')
+
+#print("\n")
+
+#print(f'------------------------------------------------------------------------')
+
 #14. Faça um programa que leia a base e a altura de um retângulo
-#    e imprima o perímetro (base + altura) e a área (base * altura / 2).
+#    e imprima o perímetro (base + altura) e a área (base * altura).
+
+
+#base= float(input ('Digite o valor da base:'))
+#altura = float(input ('Digite o valor da altura:'))
+
+#perímetro = 2 * (base + altura)
+#área = (base * altura)
+
+#print(f'O perímetro do retangualo é: {perímetro}')
+#print(f'A área do retangualo é: {área}')
+
+
+#print(f'------------------------------------------------------------------------')
 
 #15. Faça um programa que leia o valor de um produto, o percentual
 #    do desconto desejado e imprima o valor do desconto e o valor
 #    do produto subtraindo o desconto.
+
+
+#valorproduto = int(input ('Digite o valor do produto:'))
+#percentual = int(input ('Digite o valor percentual de desconto desejado:'))
+
+
+#desconto = (valorproduto * percentual / 100 )
+
+#novopreco = valorproduto - (valorproduto * percentual / 100 )
+
+#print(f'O valor do desconto é : {desconto}.')
+
+#print(f'O valor do produto subtraindo o desconto é : {novopreco}.')
+
+
+
+#print(f'------------------------------------------------------------------------')
 
 #16. Faça um programa que calcule o reajuste do salário de um
 #    funcionário. Para isso, o programa deverá ler o salário atual
 #    do funcionário e ler o percentual de reajuste. Ao final imprimir
 #    o valor do novo salário.
 
+#Salario = float(input ('Digite o valor atual do salário:'))
+#porcentagem = float(input ('Digite o valor percentual a ser acrescentado:'))
+
+
+#novosalario = Salario + (Salario * porcentagem / 100 )
+
+#print("\n")
+
+#print(f'O valor do salario com {porcentagem} % de aumento é : R$ {novosalario}.')
+
+#print("\n")
+
+
+#print(f'------------------------------------------------------------------------')
+
+#def q17():
+
 #17. Faça um programa que calcule a conversão entre graus centígrados
 #    e Fahrenheit. Para isso, leia o valor em centígrados e calcule
 #    com base na fórmula a seguir. Após calcular o programa deve
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
+
+
+#centígrados = float(input ('Digite o valor da temperatura em graus centígrados:'))
+
+#Fahrenheit = 32 + (9/5) * centígrados
+
+#print(f'A temperatura em graus centígrados {centígrados}° transformado em Fahrenheit é {Fahrenheit}°.')
+
+
+#print(f'------------------------------------------------------------------------')
+
+
 
 #18. Faça um programa que calcule a quantidade de litros de combustível
 #    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
@@ -163,6 +289,22 @@ num2 =
 #    Ao final, o programa deverá imprimir a distância percorrida e a
 #    quantidade de litros consumidos na viagem.
 
+
+#tempo = int(input ('Digite o tempo gasto na viagem em minutos:'))
+#velocidademedia = int(input ('Digite a velocidade média usada durante a viagem em km/h:'))
+
+#Distancia = (tempo/60 * velocidademedia)
+
+#combustiveltotal =  (Distancia /  12 )
+
+#print(f'A distancia percorrida é : {Distancia} km')
+#print(f'A quantidade de litros consumidos na viagem é: {combustiveltotal}Litros')
+
+
+
+#print(f'------------------------------------------------------------------------')
+
+
 #19. Faça um programa que calcule o valor de uma prestação em atraso.
 #    Para isso, o programa deve ler o valor da prestação vencida, a
 #    taxa periódica de juros e o período de atraso. Ao final, o
@@ -170,6 +312,44 @@ num2 =
 #    de atraso, os juros que serão cobrados pelo período de atraso, o
 #    valor da prestação acrescido dos juros. Considere juros simples.
 
+
+#valor = float(input ('Digite o valor da prestação:'))
+#taxa = float(input ('Digite o valor da taxa:'))
+#tempo = float(input ('Digite o tempo atrasado em dias:'))
+
+#juros = (valor * taxa/100 * tempo)
+#novovalor = (valor + juros)
+
+#print("\n")
+
+#print(f'O valor da prestação atrasada é : {valor} ')
+
+
+#print(f'O período de atraso é : {tempo} ')
+
+
+#print(f'O valor da prestação acrescido dos juros é : {novovalor} ')
+
+
+#print(f'Os juros que serão cobrados pelo período de atraso é : {juros} ')
+
+#print("\n")
+
+#print(f'------------------------------------------------------------------------')
+
 #20. Faça um programa que efetue a apresentação do valor da conversão
 #    em real (R$) de um valor lido em dólar (US$). Para isso, será
 #    necessário também ler o valor da cotação do dólar.
+
+
+
+#dolar = float(input("Informe a quantidade de dólar para conversão: U$$ "))
+#cotacao = float(input ("Informe o valor da cotação do dólar: R$ "))
+
+#conversao = dolar*cotacao
+
+#print('A quantidade de dólar convertido em real é: R$',conversao)
+
+
+
+#print(f'------------------------------------------------------------------------')
