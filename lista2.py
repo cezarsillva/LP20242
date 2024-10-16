328673971664dc105edd789ada83f4a1044402cd
CEZAR DA SILVA SOUZA <158162683+cezarsillva@users.noreply.github.com>
2024-10-16 00:11:08 +0000

    ok

--- lista2.py
+++ lista2.py
@@ -289,16 +289,16 @@ Exercícios sobre os comandos de condição em python
 #Faça um programa que leia o valor do produto e imprima o valor da venda.
 
 
-valor_produto = float(input('Digite o valor do produto: '))
+# valor_produto = float(input('Digite o valor do produto: '))
 
 
-lucro1 = valor_produto * 45 / 100
-lucro2 = valor_produto * 30 / 100
+# lucro1 = valor_produto * 45 / 100
+# lucro2 = valor_produto * 30 / 100
 
-if valor_produto < 20:
-    print(f'O valor da venda é: {valor_produto + lucro1}')
-else:
-    print(f'O valor da venda é {valor_produto + lucro2}')
+# if valor_produto < 20:
+#     print(f'O valor da venda é: {valor_produto + lucro1}')
+# else:
+#     print(f'O valor da venda é {valor_produto + lucro2}')
 
 
 
@@ -313,6 +313,26 @@ else:
 #Juvenil B 14 - 17 anos
 #Sênior maiores de 18 anos
 
+
+# idade = int(input('Digite a idade do jogador: '))
+
+
+# if idade > 0 and  idade < 5:
+#     print(f'Sem categoria.')
+# elif idade > 5 and idade < 7:
+#     print(f'Categoria Infantil A .')
+# elif idade > 8 and idade < 10:
+#     print(f'Categoria Infantil B.')
+# elif idade > 11 and idade < 13:
+#     print(f'Categoria Juvenil A.')
+# elif idade >14 and idade < 17:
+#     print(f'Categoria Juvenil B.')
+# else:
+#     print(f'Categoria senior.')
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #17. Depois da liberação do governo para as mensalidades dos planos de saúde,
 #as pessoas começaram a fazer pesquisas para descobrir um bom plano, não
 #muito caro. Um vendedor de um plano de saúde apresentou a tabela a seguir.
@@ -326,10 +346,50 @@ else:
 #Acima de 59 até 65 anos R$250,00
 #Maior que 65 anos R$400,00
 
+
+# nome = str(input('Digite o nome do beneficiario: '))
+# idade = int(input('Digite a idade do beneficiario: '))
+
+
+# if idade <= 10:
+#     print(f'O beneficiario {nome} pagara o valor de R$ 30,00.')
+# elif idade > 10 and  idade < 29:
+#     print(f'O beneficiario {nome} pagara o valor de R$ 60,00.')
+# elif idade > 29 and  idade < 45:
+#     print(f'O beneficiario {nome} pagara o valor de R$ 120,00.')
+# elif idade > 45 and  idade < 59:
+#     print(f'O beneficiario {nome} pagara o valor de R$ 150,00.')
+# elif idade > 59 and  idade < 65:
+#     print(f'O beneficiario {nome} pagara o valor de R$ 250,00.')
+# else:
+#     print(f'O beneficiario {nome} pagara o valor de R$ 400,00')
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #18. Faça um programa que leia um número inteiro entre 1 e 12 e escreva o mês
 #correspondente. Caso o usuário digite um número fora desse intervalo, deverá
 #aparecer uma mensagem informando que não existe mês com este número.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #19. Em um campeonato nacional de arco-e-flecha, tem-se equipes de três jogadores
 #para cada estado. Sabendo-se que os arqueiros de uma equipe não obtiveram o
 #mesmo número de pontos, criar um programa que informe se uma equipe foi
@@ -339,6 +399,38 @@ else:
 #• Se a soma dos pontos for maior do que 100, imprimir a média aritmética entre eles,
 #  caso contrário, imprimir a mensagem "Equipe desclassificada".
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #20. O banco XXX concederá um crédito especial com juros de 2% aos seus clientes de
 #acordo com o saldo médio no último ano. Faça um programa que leia o saldo médio
 #de um cliente e calcule o valor do crédito de acordo com a tabela a seguir.
@@ -350,6 +442,43 @@ else:
 #de 1001 a 3000 40% do valor do saldo médio
 #acima de 3001 50% do valor do saldo médio
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #21. A biblioteca de uma Universidade deseja fazer um programa que leia o nome do
 #livro que será emprestado, o tipo de usuário (professor ou aluno) e possa
 #imprimir um recibo conforme mostrado a seguir. Considerar que o professor
@@ -358,6 +487,35 @@ else:
 #• Tipo de usuário:
 #• Total de dias:
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #22. Construa um programa que leia o percurso em quilômetros, o tipo do carro e
 #informe o consumo estimado de combustível, sabendo-se que um carro tipo A faz
 #12 km com um litro de gasolina, um tipo B faz 9 km e o tipo C 8 km por litro.
@@ -371,12 +529,84 @@ else:
 #Frango         250cal Mousse diet      170cal Suco de melão     100cal
 #Carne          350cal Mousse chocolate 200cal Refrigerante diet 65cal
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #24. A polícia rodoviária resolveu fazer cumprir a lei e vistoriar veículos para
 #cobrar dos motoristas o DUT. Sabendo-se que o mês em que o emplacamento do
 #carro deve ser renovado é determinado pelo último número da placa do mesmo,
 #faça um programa que, a partir da leitura da placa do carro, informe o mês
 #em que o emplacamento deve ser renovado.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
 #25. A prefeitura contratou uma firma especializada para manter os níveis de
 #poluição considerados ideais para um país do 1º mundo. As indústrias,
 #maiores responsáveis pela poluição, foram classificadas em três grupos.
