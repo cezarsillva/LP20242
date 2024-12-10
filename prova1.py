4b44c42c4e962ec0bf75a755415e382b46da331f
CEZAR DA SILVA SOUZA <158162683+cezarsillva@users.noreply.github.com>
2024-12-10 12:43:53 +0000

    salvo

--- prova1.py
+++ prova1.py
@@ -1,72 +1,16 @@
 #Prova N1 da disciplina de Laboratório de Programação da Turma de Redes de Computadores 2024/2.
 #Aluno: cezar da silva Souza
 
-#Print('---------------------------------------------------------------------------------------------------------------------')
+print('---------------------------------------------------------------------------------------------------------------------\n')
 
 #Questão de prova 1:
 
 def q1():
+    
+    
+    nome = str(input('Digite o nome completo: '))
 
-
-nome = str(input('Digite o nome completo: '))
-
-
-
-
-
-
-
-
-
-#Questão de prova 2:
-
-def q2():
-
-
-
-
-
-
-
-
-
-
-#Questão de prova 3:
-
-def q3():
-
-
-
-
-
-
-
-
-
-
-
-
-#Questão de prova 4:
-
-def q4():
-
-
-
-
-
-
-
-
-
-
-
-#Questão de prova 5:
-
-def q5():
-
-
-
-
+    print(nome)
 
 
 
@@ -79,3 +23,5 @@ def q5():
 
 questao = int(input('Questão a ser executada: '))
 eval(f'q{questao}()')
+
+print('\n---------------------------------------------------------------------------------------------------------------------\n') 
